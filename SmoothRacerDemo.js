bccfe87db8ea4fdd145449e6965a3b812a2b4b47
nbogie <neillbogie@googlemail.com>
2018-06-11 15:22:44 +0100

    Update SmoothRacerDemo.js

--- SmoothRacerDemo.js
+++ SmoothRacerDemo.js
@@ -1,68 +1,83 @@
 let frameNum = 0
-let stepsPerPixel = 0
-let trailLength = 0
-let numPixels = 0
-let trailHyperBrightnesses: number[] = []
-let index = 0
 let playerHue = 0
+let stepsPerPixel = 0
+let drag = 0
+let trailLength = 0
+let playerBrightness = 0
+let defaultBrightness = 0
+let item: neopixel.Strip = null
+let trailHyperBrightnesses: number[] = []
+let numPixels = 0
 let pixelPos1 = 0
 let accel1 = 0
 let trailBrightnesses: number[] = []
-let defaultBrightness: number = 0
-let playerBrightness = 0
+let index = 0
 let pos1 = 0
 let vel1 = 0
-input.onButtonPressed(Button.A, () => {
-    vel1 += accel1
+radio.onDataPacketReceived( ({ receivedNumber }) =>  {
+    if (receivedNumber == 1) {
+        player1Pressed()
+    } else if (receivedNumber == 2) {
+        player2Pressed()
+    } else if (receivedNumber == 10) {
+    	
+    } else {
+        basic.showIcon(IconNames.Confused)
+    }
 })
-function updatePosition() {
+function player1Pressed()  {
+    vel1 += accel1
+}
+function updatePosition()  {
     pos1 += vel1
     pixelPos1 = pos1 / stepsPerPixel
     if (pixelPos1 >= numPixels) {
         pos1 = 0
     }
 }
-function drawPlayer() {
+input.onButtonPressed(Button.A, () => {
+    player1Pressed()
+})
+function player2Pressed()  {
+    vel1 += accel1
+}
+function drawPlayer()  {
+    radio.setGroup(22)
     item.setBrightness(defaultBrightness)
-
     playerBrightness = pins.map(
-        Math.min(Math.max(vel1, 60), 100),
-        60,
-        100,
-        40,
-        100
+    Math.min(Math.max(vel1, 60), 100),
+    60,
+    100,
+    40,
+    100
     )
     playerHue = pins.map(
-        Math.min(vel1, 60),
-        0,
-        60,
-        355,
-        420
+    Math.min(vel1, 60),
+    0,
+    60,
+    355,
+    420
     )
-    if (vel1 <= 100) {
+    if (vel1 <= 130) {
         item.setPixelColor(pixelPos1, neopixel.hsl(playerHue, 99, playerBrightness))
     } else {
-        item.setPixelWhiteLED(pixelPos1, 100)
+        item.setPixelWhiteLED(pixelPos1, 255)
     }
     drawTrail()
 }
-function drawUnderEffects() {
+function drawUnderEffects()  {
     if (vel1 >= 90) {
         item.setBrightness(pins.map(
-            vel1,
-            90,
-            200,
-            1,
-            8
+        vel1,
+        90,
+        200,
+        1,
+        8
         ))
         item.showColor(neopixel.colors(NeoPixelColors.White))
-
-        //        for (let index = 0; index < numPixels; index++) {            
-        //            item.setPixelColor(index, neopixel.colors(NeoPixelColors.White))
-        //       }
     }
 }
-function drawTrail() {
+function drawTrail()  {
     trailBrightnesses = [30, 15, 8, 4, 3, 2, 1]
     trailHyperBrightnesses = [100, 90, 70, 50, 30, 10, 4, 2, 1, 1]
     trailLength = vel1 / 10
@@ -74,17 +89,6 @@ function drawTrail() {
         }
     }
 }
-function glowWhite() {
-    for (let index3 = 0; index3 <= 255; index3++) {
-        item.setBrightness(index3)
-        item.showColor(neopixel.colors(NeoPixelColors.White))
-    }
-    for (let index4 = 255; index4 >= 0; index4--) {
-        item.setBrightness(index4)
-        item.showColor(neopixel.colors(NeoPixelColors.White))
-    }
-}
-let item: neopixel.Strip = null
 index = 0
 numPixels = 60
 item = neopixel.create(DigitalPin.P0, numPixels, NeoPixelMode.RGBW)
@@ -93,8 +97,9 @@ vel1 = 0
 accel1 = 8
 stepsPerPixel = 40
 defaultBrightness = 200
+drag = 1
 basic.forever(() => {
-    vel1 += -1
+    vel1 += 0 - drag
     if (vel1 < 0) {
         vel1 = 0
     }
