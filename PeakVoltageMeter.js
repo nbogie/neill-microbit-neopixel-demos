2cd395539d07bd2c3628f4fd461380fce4191a61
nbogie <neillbogie@googlemail.com>
2018-12-11 15:30:55 +0000

    Update PeakVoltageMeter.js

--- PeakVoltageMeter.js
+++ PeakVoltageMeter.js
@@ -1,3 +1,5 @@
+let numSamples = 0
+let currentSample = 0
 let maxVal = 0
 function calcPeakSample() {
     maxVal = 0
@@ -8,16 +10,29 @@ function calcPeakSample() {
     }
 }
 let samples: number[] = []
-samples = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+radio.setGroup(35)
+numSamples = 5
+for (let i = 0; i < numSamples; i++) {
+    samples.push(0)
+}
 basic.showIcon(IconNames.EigthNote)
-basic.forever(() => {
-    samples.push(Math.abs(pins.analogReadPin(AnalogPin.P1) - 512))
+basic.forever(function () {
+    currentSample = Math.abs(pins.analogReadPin(AnalogPin.P1) - 512)
+    samples.push(currentSample)
     samples.removeAt(0)
     calcPeakSample()
+    radio.sendNumber(maxVal)
 })
-basic.forever(() => {
-    led.plotBarGraph(
-        maxVal,
-        256
-    )
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        led.plotBarGraph(
+            Math.abs(currentSample),
+            256
+        )
+    } else {
+        led.plotBarGraph(
+            maxVal,
+            256
+        )
+    }
 })
